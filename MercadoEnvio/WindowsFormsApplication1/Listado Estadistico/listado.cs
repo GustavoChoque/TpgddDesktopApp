deb4babbe0fc92ac56c11a9222a0a54f8b6d362d
Lautaro H <Lautaro H>
2016-07-08 23:12:31 -0300

    listado estadistico

---
+++ MercadoEnvio/WindowsFormsApplication1/Listado Estadistico/listado.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1.Listado_Estadistico
+{
+    public partial class listado : Form
+    {
+        SqlDataAdapter dataAdapter = new SqlDataAdapter();
+        DataTable tablaDatos = new DataTable();
+        DbQueryHandlerListado dbQueryHandler = new DbQueryHandlerListado();
+
+        public listado()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try{
+                int anio = Convert.ToInt32(textBox1.Text.ToString());
+                if ((anio<2017) && (anio>1900))
+                {llenarTablaPorTrimestre(anio,1,4);};
+            }
+            catch{MessageBox.Show("Ingresar año válido");};
+
+        }
+
+        private void llenarTablaPorTrimestre(int anio, int mesin, int mesFin)
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter();
+                tablaDatos = new DataTable();
+                dataAdapter.SelectCommand = dbQueryHandler.vendedoresMenosVentas(anio, mesin, mesFin);
+                dataAdapter.Fill(tablaDatos);
+                dataGridView1.DataSource = tablaDatos;
+            }
+            catch { }
+        }
+
+        private void llenarTablaPorTrimestreConRubro(int anio, int mesin, int mesFin, string rubro)
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter();
+                tablaDatos = new DataTable();
+                dataAdapter.SelectCommand = dbQueryHandler.compradoresMasCompras(anio, mesin, mesFin,rubro);
+                dataAdapter.Fill(tablaDatos);
+                dataGridView1.DataSource = tablaDatos;
+            }
+            catch { }
+        }
+
+        private void llenarTablaPorAnoYmesVendMasFact(int anio, int mes)
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter();
+                tablaDatos = new DataTable();
+                dataAdapter.SelectCommand = dbQueryHandler.vendedoresMasFacturas(anio, mes);
+                dataAdapter.Fill(tablaDatos);
+                dataGridView1.DataSource = tablaDatos;
+            }
+            catch { }
+        }
+
+        private void llenarTablaPorAnoYmesVendMasMonto(int anio, int mes)
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter();
+                tablaDatos = new DataTable();
+                dataAdapter.SelectCommand = dbQueryHandler.vendedoresMasMonto(anio, mes);
+                dataAdapter.Fill(tablaDatos);
+                dataGridView1.DataSource = tablaDatos;
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int anio = Convert.ToInt32(textBox1.Text.ToString());
+                if ((anio < 2017) && (anio > 1900))
+                { llenarTablaPorTrimestre(anio, 5, 8); };
+            }
+            catch { MessageBox.Show("Ingresar año válido"); };
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int anio = Convert.ToInt32(textBox1.Text.ToString());
+                if ((anio < 2017) && (anio > 1900))
+                { llenarTablaPorTrimestre(anio, 9, 12); };
+            }
+            catch { MessageBox.Show("Ingresar año válido"); };
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                int anio = Convert.ToInt32(textBox1.Text.ToString());
+                string rubro;
+                if (comboBox1.SelectedIndex != -1) { 
+                    rubro = comboBox1.SelectedItem.ToString();
+                    if ((anio < 2017) && (anio > 1900))
+                    { llenarTablaPorTrimestreConRubro(anio, 1, 4, rubro); }
+                else { MessageBox.Show("Seleccionar rubro válido"); };
+                 };
+            }
+            catch { MessageBox.Show("Ingresar año válido"); };
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int anio = Convert.ToInt32(textBox1.Text.ToString());
+                string rubro;
+                if (comboBox1.SelectedIndex != -1)
+                {
+                    rubro = comboBox1.SelectedItem.ToString();
+                    if ((anio < 2017) && (anio > 1900))
+                    { llenarTablaPorTrimestreConRubro(anio, 1, 4, rubro); }
+                    else { MessageBox.Show("Seleccionar rubro válido"); };
+                };
+            }
+            catch { MessageBox.Show("Ingresar año válido"); };
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int anio = Convert.ToInt32(textBox1.Text.ToString());
+                string rubro;
+                if (comboBox1.SelectedIndex != -1)
+                {
+                    rubro = comboBox1.SelectedItem.ToString();
+                    if ((anio < 2017) && (anio > 1900))
+                    { llenarTablaPorTrimestreConRubro(anio, 1, 4, rubro); }
+                    else { MessageBox.Show("Seleccionar rubro válido"); };
+                };
+            }
+            catch { MessageBox.Show("Ingresar año válido"); };
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int anio = Convert.ToInt32(textBox1.Text.ToString());
+                int mes = Convert.ToInt32(comboBox2.SelectedItem.ToString());
+                llenarTablaPorAnoYmesVendMasFact(anio, mes);
+            }
+            catch { MessageBox.Show("Ingresar año y mes válidos"); };
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int anio = Convert.ToInt32(textBox1.Text.ToString());
+                int mes = Convert.ToInt32(comboBox3.SelectedItem.ToString());
+                llenarTablaPorAnoYmesVendMasMonto(anio, mes);
+            }
+            catch { MessageBox.Show("Ingresar año y mes válidos"); };
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        
+    }
+
+    class DbQueryHandlerListado 
+    {
+        public SqlCommand vendedoresMenosVentas(int añobusacod, int mesinit,int mesfin) 
+        {
+            SqlCommand comando = new SqlCommand(
+            @"select TOP 5 COUNT(P.Publicacion_Stock), U.Username, c.Cli_Nombre, c.Cli_Apellido 
+            from GROUP_APROVED.Clientes c join GROUP_APROVED.Usuarios u on c.Id_Usuario = u.Id_Usr 
+            join GROUP_APROVED.Publicaciones p ON U.Id_Usr= P.Id_Usuario left 
+            join GROUP_APROVED.Visibilidades V ON P.Visibilidad_Cod = v.Visibilidad_Cod
+            WHERE year(p.Publicacion_Fecha) = "+añobusacod +
+            " and month(p.Publicacion_Fecha) between "+mesinit+
+            " and "+mesfin+@" and v.Visibilidad_Porcentaje between 0.15 and 0.30
+            GROUP BY U.Username,c.Cli_Nombre, c.Cli_Apellido
+            order by 1 DESC,  avg (v.Visibilidad_Porcentaje)", DbConnection.connection.getdbconnection());
+            return comando;
+
+        }
+
+        public SqlCommand compradoresMasCompras(int añobusacod, int mesinit,int messfin, string rubroelegido) 
+        {
+            SqlCommand comando = new SqlCommand(
+            @"select TOP 5 u.Username, count(Compra_Cantidad), c1.Cli_Apellido, c1.Cli_Nombre from GROUP_APROVED.Clientes c1 
+            join GROUP_APROVED.Usuarios u on c1.Id_Usuario = u.Id_Usr 
+            join GROUP_APROVED.Compras c on u.Id_Usr = c.Id_Usuario 
+            left join GROUP_APROVED.Publicaciones p on c.Publicacion_Cod = p.Publicacion_Cod 
+            join GROUP_APROVED.Rubros r on p.Id_Rubro = r.Id_Rubro
+            where year(c.Compra_Fecha) =" + añobusacod +
+            "and month(c.Compra_Fecha) between " + mesinit + " and " + messfin +
+            "and r.Rubro_Desc_Corta = " + rubroelegido +
+            @"group by u.Username, c1.Cli_Apellido, c1.Cli_Nombre
+            order by 2 DESC ", DbConnection.connection.getdbconnection());
+            return comando;
+        }
+
+        public SqlCommand vendedoresMasFacturas(int añobusacod, int mesbuscado) 
+        {
+            SqlCommand comando = new SqlCommand(
+            @"select TOP 5 u.Username, c.Cli_Apellido, c.Cli_Nombre, count(f.Nro_Fact) 
+            from GROUP_APROVED.Clientes c join GROUP_APROVED.Usuarios u on c.Id_Usuario = u.Id_Usr 
+            join GROUP_APROVED.Publicaciones p ON U.Id_Usr= P.Id_Usuario 
+            join GROUP_APROVED.Facturas f on p.Publicacion_Cod = f.Publicacion_Cod
+            where year(f.Fact_Fecha) = "+añobusacod+" and month(f.Fact_Fecha) = "+mesbuscado+
+            @"group by u.Username, c.Cli_Apellido, c.Cli_Nombre
+            order by 4 DESC", DbConnection.connection.getdbconnection());
+            return comando;
+        }
+
+        public SqlCommand vendedoresMasMonto(int añobusacod, int mesbuscado) 
+        {
+            SqlCommand comando = new SqlCommand(
+            @"select TOP 5 u.Username, c.Cli_Apellido, c.Cli_Nombre, SUM(f.Fact_Total) 
+            from GROUP_APROVED.Clientes c join GROUP_APROVED.Usuarios u on c.Id_Usuario = u.Id_Usr 
+            join GROUP_APROVED.Publicaciones p ON U.Id_Usr= P.Id_Usuario 
+            join GROUP_APROVED.Facturas f on p.Publicacion_Cod = f.Publicacion_Cod
+            where year(f.Fact_Fecha) = "+añobusacod+" and month(f.Fact_Fecha) = "+mesbuscado+
+            @"group by u.Username, c.Cli_Apellido, c.Cli_Nombre
+            order by 4 DESC", DbConnection.connection.getdbconnection());
+            return comando;
+        }
+    }
+}
---
+++ MercadoEnvio/WindowsFormsApplication1/Listado Estadistico/listado.designer.cs
@@ -0,0 +1,353 @@
+﻿namespace WindowsFormsApplication1.Listado_Estadistico
+{
+    partial class listado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 198);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(655, 197);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Consultar TOP 5 del año:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(286, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Vendedores con mayor cantidad de productos no vendidos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(319, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Clientes con mayor cantidad de productos comprados, en el rubro:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Clientes con mayor cantidad de facturas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Clientes con mayor monto facutrado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(438, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "1er Trimestre";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(517, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "2do Trimestre";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(596, 53);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "3er Trimestre";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(438, 81);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "1er Trimestre";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(517, 81);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "2do Trimestre";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(596, 81);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "3er Trimestre";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(438, 109);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Mostrar";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(438, 138);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(80, 23);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "Mostrar";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(482, 417);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 18;
+            this.button13.Text = "Volver";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 20);
+            this.textBox1.TabIndex = 19;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A /",
+            "A P",
+            "Aor",
+            "Car",
+            "Cas",
+            "Cla",
+            "Cne",
+            "Cta",
+            "Fop",
+            "G",
+            "HTh",
+            "iy ",
+            "MMP",
+            "O c",
+            "P, ",
+            "Pct",
+            "Prr",
+            "Rdu",
+            "Sid",
+            "Ste",
+            "Tis"});
+            this.comboBox1.Location = new System.Drawing.Point(343, 81);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(77, 21);
+            this.comboBox1.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(308, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "mes:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboBox2.Location = new System.Drawing.Point(343, 110);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(77, 21);
+            this.comboBox2.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(308, 143);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "mes:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboBox3.Location = new System.Drawing.Point(343, 140);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(77, 21);
+            this.comboBox3.TabIndex = 24;
+            // 
+            // listado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 452);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "listado";
+            this.Text = "Listado Estadístico";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox3;
+    }
+}
